aefff5b173f8b90e5faa46b080b1e77ab1039767
win <win@win-PC>
2016-09-24 19:21:06 -0500

    Estuve trabajándole a agregar contacto pero no me quiere funcionar.

--- src/BusinessLogic/ContactsBook.java
+++ src/BusinessLogic/ContactsBook.java
@@ -3,6 +3,8 @@ package BusinessLogic;
 
 import Data.Contact;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
 
 
 
@@ -11,13 +13,41 @@ import java.util.HashMap;
 public class ContactsBook {
     
     public static HashMap<Integer,Contact> contacts;     
+    static Contact contacto[];
     
    public static void addContact(){
+       String nombre;
+       String apellido;
+       HashSet<String> correosElectronicos;
+       int telefonoFijo;
+       long telefonoMovil;
+       String direccion;
+       int contador = 0;
+       Scanner reader = new Scanner (System.in);
        
-       
-    }
+    
+       while (contacto [contador] != null && contador<contacto.length){
+			contador++;
+		if (contador<contacto.length){
+			System.out.print("Nombre: ");
+			nombre = reader.nextLine();
+			System.out.print("Apellido: ");
+                        apellido = reader.nextLine();
+                        System.out.print("Correo Electronicos: ");
+                        // Cómo llenar los correos?
+			System.out.print("Teléfono fijo: ");
+			telefonoFijo = reader.nextInt();
+			System.out.print("Teléfono Móvil: ");
+			telefonoMovil = reader.nextInt();
+			System.out.print("Dirección: ");
+			direccion = reader.nextLine();
+                }
+       }
+} 
+    
    
    public static void removeContact(){
+       
        
    }
    public static void updateContact(){
@@ -36,8 +66,7 @@ public class ContactsBook {
     
     public static void main (String [] args){
         
-        
-        
+        addContact();
     }
     
 }
