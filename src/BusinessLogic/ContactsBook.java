e4ac92d5bdda6378034b72ff63206de5a65668b4
Nicolas-GR <nguerreror@unal.edu.co>
2016-09-25 15:05:50 -0500

    Cambios en UI y Business, estable aún

--- src/BusinessLogic/ContactsBook.java
+++ src/BusinessLogic/ContactsBook.java
@@ -1,43 +1,64 @@
-
 package BusinessLogic;
 
 import Data.Contact;
+import UI.ContactsBookUI;
 import java.util.HashMap;
 
-
-
-
-
 public class ContactsBook {
-    
-    public static HashMap<Integer,Contact> contacts;     
-    
-   public static void addContact(){
-       
-       
+
+    static HashMap<String, Contact> contacts;
+
+    public static void main(String[] args) {
+        ContactsBookUI.imprimeBienvenida();
+        
+        for(;;){
+            ContactsBookUI.imprimeOpciones();
+            int  opcionElegida = ContactsBookUI.validaOpciones();
+            
+            switch(opcionElegida){
+                case 1: addContact();
+                        break;
+                case 2: removeContact();
+                        break;
+                case 3: updateContact();
+                        break;
+                case 4: printAllContacts();
+                        break;
+                case 5: removeContact();
+                        break;
+                case 6: removeContact();
+                        break;
+                
+            }
+            
+            
+        }
     }
-   
-   public static void removeContact(){
-       
-   }
-   public static void updateContact(){
-       
-   }
-   public static void printAllContacts(HashMap<Integer,Contact> contacts){
-       
-       
-       }       
-   
-   public static void printContactsKeys(){
-       
-   }
-   
-        
+
+
+/////////////////////////////////////////////////////////////
     
-    public static void main (String [] args){
-        
-        
-        
+    
+    public static void addContact() {
+
     }
-    
+
+    public static void removeContact() {
+
+    }
+
+    public static void updateContact() {
+
+    }
+
+    public static void printAllContacts() {
+
+    }
+
+    public static void printContactsKeys() {
+
+    }
+
+
+
 }
--- src/UI/ContactsBookUI.java
+++ src/UI/ContactsBookUI.java
@@ -1,8 +1,6 @@
-
 package UI;
-
+import java.util.Scanner;
 import Data.Contact;
-
 
 public class ContactsBookUI {
     
@@ -10,4 +8,19 @@ public class ContactsBookUI {
         System.out.println(contact);
     }
     
+    public static void imprimeBienvenida(){
+        System.out.println("Bienvenidio a su Libro de Contactos");
+        System.out.println("###################################");
+    }
+    
+    public static void imprimeOpciones(){
+        System.out.println("¿Qué desea hacer?");
+        System.out.print("[1] Agregar \n[2] Eliminar \n[3] Actualizar \n[4] Mostrar Todos \n[5] Mostrar Uno \n[6] Salir\n");
+    }
+    
+    public static int validaOpciones(){
+        Scanner entrada = new Scanner(System.in);
+        int opcion = entrada.nextInt();
+        return opcion;
+    }
 }
