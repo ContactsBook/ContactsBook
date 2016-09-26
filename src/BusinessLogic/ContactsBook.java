01f5b34de1779d93f1b5bc5ce29f422c96c31ba3
Nicolas-GR <nguerreror@unal.edu.co>
2016-09-25 21:09:41 -0500

    Metodo printAllContacts funcionando correctamente

--- src/BusinessLogic/ContactsBook.java
+++ src/BusinessLogic/ContactsBook.java
@@ -3,61 +3,67 @@ package BusinessLogic;
 import Data.Contact;
 import UI.ContactsBookUI;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class ContactsBook {
-    
+
     //static HashMap<String,Contact> contacts;
     static int contador = 0; //laves  :v
 
     public static void main(String[] args) {
-        
-        HashMap<String,Contact> listaContactos;
+
+        HashMap<String, Contact> listaContactos;
         listaContactos = new HashMap();
-        
+
         ContactsBookUI.imprimeBienvenida();
         boolean control = true;
-        
-        while(control){
+
+        while (control) {
             ContactsBookUI.imprimeOpciones();
-            
-            int  opcionElegida = ContactsBookUI.pedirInt();
-            
-            switch(opcionElegida){
-                case 1: addContact(listaContactos);
-                        break;
-                case 2: String key = String.valueOf(ContactsBookUI.pedirInt());
-                        removeContact(listaContactos, key);
-                        break;
-                case 3: updateContact();
-                        break;
-                case 4: printAllContacts();
-                        break;
-                case 5: printContactsKeys();
-                        break;
-                case 6: control = false;
-                        break;
-                default: ContactsBookUI.error();
-                        break;
-                
+
+            int opcionElegida = ContactsBookUI.pedirInt();
+
+            switch (opcionElegida) {
+                case 1:
+                    addContact(listaContactos);
+                    break;
+                case 2:
+                    String key = String.valueOf(ContactsBookUI.pedirInt());
+                    removeContact(listaContactos, key);
+                    break;
+                case 3:
+                    updateContact();
+                    break;
+                case 4:
+                    printAllContacts(listaContactos);
+                    break;
+                case 5:
+                    printContactsKeys();
+                    break;
+                case 6:
+                    control = false;
+                    break;
+                default:
+                    ContactsBookUI.error();
+                    break;
+
             }
-            
+
         }
     }
 
-
 /////////////////////////////////////////////////////////////
-    
-    
-    public static void addContact(HashMap<String,Contact> listaContactos) {
-        contador ++;
-        String key  = String.valueOf(contador);
+    public static void addContact(HashMap<String, Contact> listaContactos) {
+        contador++;
+        String key = String.valueOf(contador);
         Contact newContact;
         newContact = ContactsBookUI.crearContacto();
         listaContactos.put(key, newContact);
-        
+
     }
 
-    public static void removeContact(HashMap<String,Contact> listaContactos , String key) {
+    public static void removeContact(HashMap<String, Contact> listaContactos, String key) {
         Contact deletedContact = listaContactos.remove(key);
     }
 
@@ -65,17 +71,14 @@ public class ContactsBook {
 
     }
 
-    public static void printAllContacts(/*HashMap<String,Contact> contactos*/) {
-        
-        /*for (Contact contacto : contactos){
-            
-        }*/
+    public static void printAllContacts(HashMap<String, Contact> contactos) {
+
+        ContactsBookUI.recorreImprime(contactos);
+
     }
 
     public static void printContactsKeys() {
 
     }
-
-
 
 }
--- src/UI/ContactsBookUI.java
+++ src/UI/ContactsBookUI.java
@@ -1,87 +1,97 @@
 package UI;
+
 import java.util.Scanner;
 import Data.Contact;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
 
 public class ContactsBookUI {
     //static Scanner entrada = new Scanner(System.in);
-    
-    
-    public static void printContact (Contact contact){
+
+    public static void printContact(Contact contact) {
         System.out.println(contact);
     }
-    
-    public static void imprimeBienvenida(){
+
+    public static void imprimeBienvenida() {
         System.out.println("Bienvenidio a su Libro de Contactos");
         System.out.println("###################################");
     }
-    
-    public static void imprimeOpciones(){
+
+    public static void imprimeOpciones() {
         System.out.println("¿Qué desea hacer?");
         System.out.print("[1] Agregar \n[2] Eliminar \n[3] Actualizar \n[4] Mostrar Todos \n[5] Mostrar Uno \n[6] Salir\n");
     }
-    
-    public static int pedirInt(){
+
+    public static int pedirInt() {
         Scanner entrada = new Scanner(System.in);
         int opcion = entrada.nextInt();
         return opcion;
     }
-    
-    public static String pedirCadena(){
+
+    public static String pedirCadena() {
         Scanner entrada = new Scanner(System.in);
         String cadena = entrada.nextLine();
         return cadena;
     }
-    /*public static Contact instanciaContacto(Contact nuevoContacto){
-        return (nuevoContacto = new Contact());
-    }*/
-    
-    public static Contact crearContacto(){
-        
+
+    public static Contact crearContacto() {
+
         Contact newContact;
-        
+
         System.out.println("Escriba el nombre del contacto");
         String nombre = pedirCadena();
-        
+
         System.out.println("Escriba el apellido del contacto");
         String apellido = pedirCadena();
-        
+
         HashSet emails;
         emails = crearListaCorreos();
-        
+
         System.out.println("Escriba el telefono fijo del contacto");
         String fijoStr = pedirCadena();
         int fijo = Integer.parseInt(fijoStr);
-        
+
         System.out.println("Escriba el telefono móvil del contacto");
         String movilStr = pedirCadena();
         long movil = Long.parseLong(movilStr);
-        
+
         System.out.println("Escriba la direccion del contacto");
         String dir = pedirCadena();
-        
+
         newContact = new Contact(nombre, apellido, emails, fijo, movil, dir);
-        
+
         return newContact;
-        
+
     }
-    
-    public static HashSet crearListaCorreos(){
+
+    public static HashSet crearListaCorreos() {
         HashSet<String> emails = new HashSet();
-               
+
         System.out.print("Ingrese el número de correos a guardar: ");
         int contador = pedirInt();
-        
-        for (int i = 1; i<=contador; i++){
-            System.out.println("Escriba el e-mail " +i+ " del contacto");
+
+        for (int i = 1; i <= contador; i++) {
+            System.out.println("Escriba el e-mail " + i + " del contacto");
             emails.add(pedirCadena());
         }
-        
-        
+
         return emails;
     }
-    
-    public static void error(){
+
+    public static void error() {
         System.out.println("Por  favor ingrese una opcion válida");
+    }
+
+    public static void recorreImprime(HashMap<String,Contact> contactos) {
+        if(contactos.isEmpty()){
+            System.out.println("No hay contactos que mostrar");
+        }
+        Iterator it = contactos.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry e = (Map.Entry) it.next();
+            System.out.println("Key: " + e.getKey() + " " + e.getValue());
+        }
     }
 }
